fe10a9927736266c12bee4f4adf6ed0e12d90d92
AlexZhu123 <61170823+AlexZhu123@users.noreply.github.com>
2020-07-30 12:34:31 +1000

    hello

--- Group3v2/app/src/main/java/com/example/infs3605group3application/fragments/LoginFragment.java
+++ Group3v2/app/src/main/java/com/example/infs3605group3application/fragments/LoginFragment.java
@@ -34,7 +34,7 @@ public class LoginFragment extends Fragment {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), AnswerQuestions.class);
+                Intent intent = new Intent(getActivity(), MakePost.class);
                 startActivity(intent);
             }
         });
